38f517005fa1875dda9fff547d7f6ebd8672cb3d
Matt Wagner <m.wagner@dwdiesel.com>
2017-10-20 10:22:49 -0400

    Protect child (nested) routes with canActivate
    This allows the servers route to be accessed, but not its children routes.

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -2,7 +2,8 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Router
+  Router,
+  CanActivateChild
 } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
@@ -10,7 +11,7 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
@@ -26,5 +27,11 @@ export class AuthGuard implements CanActivate {
         }
       }
       );
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
